9ee23142d2387fc0c2c069361e2d4521d1a15cd2
Omp1 <evan3bal@gmail.com>
2023-07-16 13:22:18 +0300

    Install script created

---
+++ ip-alert/install.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+pip install requests socket
+mkdir -p /home/grafana/ip-alert/
+cp ip-alert.py /home/grafana/ip-alert/
+
+echo "[Unit]
+Description=Starts the ip alert system
+
+[Service]
+ExecStart=/usr/bin/python3 /home/grafana/ip-alert/ip-alert.py
+Type=simple
+" > /lib/systemd/system/ip-alert.service
+systemctl daemon-reload
+systemctl enable ip-alert.service
+systemctl start ip-alert.service
+
+chown -R grafana /home/grafana/ip-alert/
